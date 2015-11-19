1bf2547767a13057af60acc0daf22fcf4cde55d9
jingjinjingjin <1220388772@qq.com>
2015-11-19 19:59:38 +0800

    2015-11-19 20:00

--- Application/Admin/Controller/CustomController.class.php
+++ Application/Admin/Controller/CustomController.class.php
@@ -837,65 +837,42 @@ class CustomController extends AdminController {
 	public function customListImport(){
 		if(IS_POST){
 			if (!empty($_FILES)) {
-				import("@.ORG.UploadFile");
 				$config=array(
 						'allowExts'=>array('xlsx','xls'),
-						'savePath'=>'./Public/upload/',
+						'savePath'=>'Public/upload/',
 						'saveRule'=>'time',
 				);
-				$upload = new UploadFile($config);
-				if (!$upload->upload()) {
-					$this->error($upload->getErrorMsg());
+				
+				$upload = new Upload($config);
+				$info = $upload->upload();
+				if (!$info) {
+					$this->error($upload->getError());
 				} else {
-					$info = $upload->getUploadFileInfo();
-			
+					//$this->success('上传成功！');
 				}
-			
-				vendor("PHPExcel.PHPExcel");
-				$file_name=$info[0]['savepath'].$info[0]['savename'];
-				$objReader = PHPExcel_IOFactory::createReader('Excel5');
+				
+				Vendor("PHPExcel.PHPExcel");
+				Vendor("PHPExcel.PHPExcel.IOFactory");
+				$file_name=$upload->rootPath . $info['import']['savepath'].$info['import']['savename'];
+				$path = $upload->rootPath .$info['import']['savepath'];
+				$objReader = \PHPExcel_IOFactory::createReader('Excel5');
 				$objPHPExcel = $objReader->load($file_name,$encode='utf-8');
 				$sheet = $objPHPExcel->getSheet(0);
 				$highestRow = $sheet->getHighestRow(); // 取得总行数
 				$highestColumn = $sheet->getHighestColumn(); // 取得总列数
 				for($i=3;$i<=$highestRow;$i++)
 				{
-					$data['account']= $data['truename'] = $objPHPExcel->getActiveSheet()->getCell("B".$i)->getValue();
-					$sex = $objPHPExcel->getActiveSheet()->getCell("C".$i)->getValue();
-					// $data['res_id']    = $objPHPExcel->getActiveSheet()->getCell("D".$i)->getValue();
-					$data['class'] = $objPHPExcel->getActiveSheet()->getCell("E".$i)->getValue();
-					$data['year'] = $objPHPExcel->getActiveSheet()->getCell("F".$i)->getValue();
-					$data['city']= $objPHPExcel->getActiveSheet()->getCell("G".$i)->getValue();
-					$data['company']= $objPHPExcel->getActiveSheet()->getCell("H".$i)->getValue();
-					$data['zhicheng']= $objPHPExcel->getActiveSheet()->getCell("I".$i)->getValue();
-					$data['zhiwu']= $objPHPExcel->getActiveSheet()->getCell("J".$i)->getValue();
-					$data['jibie']= $objPHPExcel->getActiveSheet()->getCell("K".$i)->getValue();
-					$data['honor']= $objPHPExcel->getActiveSheet()->getCell("L".$i)->getValue();
-					$data['tel']= $objPHPExcel->getActiveSheet()->getCell("M".$i)->getValue();
-					$data['qq']= $objPHPExcel->getActiveSheet()->getCell("N".$i)->getValue();
-					$data['email']= $objPHPExcel->getActiveSheet()->getCell("O".$i)->getValue();
-					$data['remark']= $objPHPExcel->getActiveSheet()->getCell("P".$i)->getValue();
-					$data['sex']=$sex=='男'?1:0;
-					$data['res_id'] =1;
-			
-					$data['last_login_time']=0;
-					$data['create_time']=$data['last_login_ip']=$_SERVER['REMOTE_ADDR'];
-					$data['login_count']=0;
-					$data['join']=0;
-					$data['avatar']='';
-					$data['password']=md5('123456');
-					M('Member')->add($data);
-						
+					$data['id']    = $objPHPExcel->getActiveSheet()->getCell("A".$i)->getValue();
+					$data['class'] = $objPHPExcel->getActiveSheet()->getCell("B".$i)->getValue();
+					M('Test')->add($data);
 				}
 				$this->success('导入成功！');
-			}else
-			{
+			}else{
 				$this->error("请选择上传的文件",U('Custom/customListImport'));
 			}
 		}
 		$this->display();
 	}
-	
 	
 	/* 共享客户列表 */
 	public function shareCustomList(){
